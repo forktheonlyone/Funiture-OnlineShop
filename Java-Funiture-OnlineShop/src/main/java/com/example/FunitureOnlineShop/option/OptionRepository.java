98a2d8900de58f7d7a0b4d939436f7406b90c55c
LAH4285 <ahyun4285@gmail.com>
2023-12-12 11:46:58 +0900

    Option directory

---
+++ Java-Funiture-OnlineShop/src/main/java/com/example/FunitureOnlineShop/option/OptionRepository.java
@@ -0,0 +1,9 @@
+package com.example.FunitureOnlineShop.option;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+    List<Option> findByProductId(Long id);
+}
