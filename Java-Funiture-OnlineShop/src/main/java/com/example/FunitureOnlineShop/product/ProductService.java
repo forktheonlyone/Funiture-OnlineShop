6ae668809e1711748ec27490207f190b07cfa09e
forktheonlyone <forktheonlyone@gmail.com>
2023-12-12 12:38:07 +0900

    [UPDATE] 상품의 추가 기능 구현
    카테고리 id를 설정하여 신규 상품 추가 기능

--- Java-Funiture-OnlineShop/src/main/java/com/example/FunitureOnlineShop/product/ProductService.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/FunitureOnlineShop/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.FunitureOnlineShop.product;
 
+import com.example.core.error.exception.Exception404;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,11 +10,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ProductService {
     private ProductRepository productRepository;
-/*
-    public void save(Product product) {
+    private CategoryRepository categoryRepository;
+
+    @Transactional
+    public void save(ProductResponse.FindByCategoryIdDTO productResponseFind) {
+        Category category = categoryRepository.findById(productResponseFind.getCategoryId())
+                .orElseThrow( () -> new Exception404(
+                        "해당 카테고리를 찾을 수 없습니다. : " + productResponseFind.getCategoryId()));
+        Product product = productResponseFind.toEntity(category);
         productRepository.save(product);
-
-
     }
-    */
+
 }
