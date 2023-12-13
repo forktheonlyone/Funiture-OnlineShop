521c5aa88467373669bd740508e98d4dc7512d0c
forktheonlyone <forktheonlyone@gmail.com>
2023-12-13 17:49:26 +0900

    [UPDATE] 카테고리 찾기 기능 구현
    등록된 카테고리의 ID를 조회해 특정 상품 찾기, 등록된 카테고리들 조회

--- Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/category/CategoryController.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/category/CategoryController.java
@@ -3,9 +3,9 @@ package com.example.funitureOnlineShop.category;
 import com.example.funitureOnlineShop.core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,10 +13,27 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    // 카테고리 저장
     @PostMapping("/category/save")
     public ResponseEntity<?> save(@RequestBody CategoryResponse categoryResponse) {
         categoryService.save(categoryResponse);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(categoryResponse);
         return ResponseEntity.ok(categoryResponse);
+    }
+
+    // 등록된 카테고리 모두 조회
+    @GetMapping("/categories")
+    public ResponseEntity<?> findAll() {
+        List<Category> categories = categoryService.findAll();
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(categories);
+        return ResponseEntity.ok(apiResult);
+    }
+
+    // 등록된 카테고리들의 id번호로 특정한 카테고리 조회
+    @GetMapping("/category/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        Category category = categoryService.findById(id);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(category);
+        return ResponseEntity.ok(apiResult);
     }
 }
