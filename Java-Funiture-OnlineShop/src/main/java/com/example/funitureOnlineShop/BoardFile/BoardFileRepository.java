82014433bd752acc7f73c4e2d2a0f6bb52a19938
LAH4285 <ahyun4285@gmail.com>
2023-12-13 12:40:33 +0900

    [UPDATE] 보드 저장 및 읽기 기능

--- Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/BoardFile/BoardFileRepository.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/BoardFile/BoardFileRepository.java
@@ -1,6 +1,10 @@
 package com.example.funitureOnlineShop.BoardFile;
 
+import com.example.funitureOnlineShop.Board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
+    List<BoardFile> findByBoardId(Long boardId);
 }
