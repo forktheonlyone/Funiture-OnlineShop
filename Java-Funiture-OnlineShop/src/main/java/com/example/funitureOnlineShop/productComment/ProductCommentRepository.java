a6ee2d24e193a22198eca04358773485971002ff
mearae <qownsbjh@gmail.com>
2023-12-13 11:30:34 +0900

    [CREATE] 상품평 생성
    상품평(ProductComment) 엔티티 생성 및 관련 컨트롤러, 서비스, 레포지토리 생성

---
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/productComment/ProductCommentRepository.java
@@ -0,0 +1,7 @@
+package com.example.funitureOnlineShop.productComment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductCommentRepository extends JpaRepository<ProductComment, Long> {
+
+}
