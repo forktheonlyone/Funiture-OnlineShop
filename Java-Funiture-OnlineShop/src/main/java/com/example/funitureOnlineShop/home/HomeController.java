f264eb44fad9626d37fc8557ce06c1a1e7e65df1
forktheonlyone <forktheonlyone@gmail.com>
2023-12-14 13:06:10 +0900

    [UPDATE] 홈컨트롤러 장바구니 페이지 구현
    장바구니에 상품이 담겨있다면 담겨있는 상품들을 보여주고 없다면 메시지 출력

--- Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/home/HomeController.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/home/HomeController.java
@@ -1,9 +1,13 @@
 package com.example.funitureOnlineShop.home;
 
+import com.example.funitureOnlineShop.cart.CartResponse;
+import com.example.funitureOnlineShop.cart.CartService;
 import com.example.funitureOnlineShop.core.error.exception.Exception404;
+import com.example.funitureOnlineShop.core.security.CustomUserDetails;
 import com.example.funitureOnlineShop.product.Product;
 import com.example.funitureOnlineShop.product.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +20,15 @@ import java.util.List;
 public class HomeController {
 
     private final ProductService productService;
+    private final CartService cartService;
 
     // 메인 홈페이지
     @GetMapping(value = {"/", ""})
     public String home() {
         return "index";
     }
+
+    // !!----------< 상품 관련 페이지 > -----------
 
     // 전체 상품 확인
     @GetMapping("/product")
@@ -37,5 +44,18 @@ public class HomeController {
         List<Product> products = productService.findByCategory(id);
         model.addAttribute("products", products);
         return "productCategoryPage";
+    }
+
+    // !!----------< 장바구니 관련 페이지 > -----------
+
+    @GetMapping("/cart")
+    public String showCart(Model model, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        try{
+            CartResponse.FindAllDTO cart = cartService.findAll(customUserDetails.getUser());
+            model.addAttribute("cart", cart);
+        }catch (Exception e){
+            model.addAttribute("message", "장바구니가 비어 있습니다.");
+        }
+        return "cartPage";
     }
 }
