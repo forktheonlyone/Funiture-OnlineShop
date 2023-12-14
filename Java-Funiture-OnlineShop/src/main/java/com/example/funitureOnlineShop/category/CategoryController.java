1e2b77953ba5f7b90a1e99deada3f196b0452ad8
LAH4285 <ahyun4285@gmail.com>
2023-12-14 13:09:31 +0900

    Squashed commit of the following:
    commit cac31f9e8ccc1851c1d6bf89cedc761e44775fd6
    Merge: 866b030 730e086
    Author: Fork <forktheonlyone@gmail.com>
    Date:   Thu Dec 14 12:48:55 2023 +0900
    
        [FIX] 상품 서비스 로직 변경
    
        예외처리 추가
    
    commit 730e086d6e0f1acf6c5815398dc978d81d75942e
    Author: forktheonlyone <forktheonlyone@gmail.com>
    Date:   Thu Dec 14 12:48:32 2023 +0900
    
        [FIX] 상품 서비스 로직 변경
    
        예외처리 추가
    
    commit 866b0300396b504c3100e1ea187b3e0b82d67028
    Merge: 3e7ec12 9c93700
    Author: Fork <forktheonlyone@gmail.com>
    Date:   Thu Dec 14 12:46:26 2023 +0900
    
        [UPDATE] 카테고리 삭제 기능 추가
    
        카테고리 삭제
    
    commit 9c93700a84c066772cc18d09954ed066505a703f
    Author: mearae <qownsbjh@gmail.com>
    Date:   Thu Dec 14 12:44:26 2023 +0900
    
        [UPDATE] 카테고리 삭제
    
        카테고리 삭제
    
    commit 3e7ec12e8cdeafa29c0298d8ab535cd66624a2d5
    Merge: ab86ea4 14a0983
    Author: Fork <forktheonlyone@gmail.com>
    Date:   Thu Dec 14 12:42:47 2023 +0900
    
        [UPDATE] 관리자가입 및 유저정보확인
    
        관리자 회원가입 및 유저 정보확인 기능입니다
    
    commit ab86ea46cbe6de0ed6587d1e43f4849eb8d1acf1
    Merge: 3707552 19be90d
    Author: Fork <forktheonlyone@gmail.com>
    Date:   Thu Dec 14 12:42:12 2023 +0900
    
        [UPDATE] 카테고리 수정 기능 추가
    
        카테고리의 이름과 그 상위 카테고리 수정
    
    commit 3707552cf6bacdeddfcc650fe23b4adad3ad99c4
    Merge: 4756528 b7ab2bf
    Author: Fork <forktheonlyone@gmail.com>
    Date:   Thu Dec 14 12:41:41 2023 +0900
    
        [FIX] 상품 매핑방식 수정
    
        POST -> GET, DELETE, PUT
    
    commit 19be90d14edeae91cb12d8badfa781257f66bec4
    Merge: ecbfad1 4756528
    Author: mearae <qownsbjh@gmail.com>
    Date:   Thu Dec 14 12:22:31 2023 +0900
    
        Merge branch 'main' into bae
    
    commit ecbfad156fa47114a6e45fb497a883522d6c76f0
    Author: mearae <qownsbjh@gmail.com>
    Date:   Thu Dec 14 12:17:30 2023 +0900
    
        [UPDATE] 카테고리 수정 기능 추가
    
        카테고리의 이름과 그 상위 카테고리 수정
    
    commit b7ab2bfcfd255f39205ded85489341083790ac6b
    Author: forktheonlyone <forktheonlyone@gmail.com>
    Date:   Thu Dec 14 12:10:40 2023 +0900
    
        [FIX] 상품 매핑방식 수정
    
        POST -> GET, DELETE, PUT

--- Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/category/CategoryController.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/category/CategoryController.java
@@ -21,13 +21,14 @@ public class CategoryController {
     public ResponseEntity<?> save(@RequestBody @Valid CategoryRequest.SaveDto saveDto) {
         categoryService.save(saveDto);
 
-        return ResponseEntity.ok(ApiUtils.success(null);
+        return ResponseEntity.ok(ApiUtils.success(null));
     }
 
     // 등록된 카테고리 모두 조회
     @GetMapping("/categories")
     public ResponseEntity<?> findAll() {
         List<CategoryResponse.FindAllDto> categories = categoryService.findAll();
+
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(categories);
         return ResponseEntity.ok(apiResult);
     }
@@ -36,7 +37,24 @@ public class CategoryController {
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         CategoryResponse.FindByIdDto categoryDto = categoryService.findById(id);
+
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(categoryDto);
         return ResponseEntity.ok(apiResult);
+    }
+
+    // 카테고리의 이름과 그 상위 카테고리 수정
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@ModelAttribute CategoryRequest.UpdateDto updateDto ) {
+        categoryService.update(updateDto);
+
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
+
+    // 카테고리 삭제
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        categoryService.delete(id);
+
+        return ResponseEntity.ok(ApiUtils.success(null));
     }
 }
--- Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/product/ProductController.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/product/ProductController.java
@@ -3,10 +3,7 @@ package com.example.funitureOnlineShop.product;
 import com.example.funitureOnlineShop.core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -18,7 +15,7 @@ public class ProductController {
     private final ProductService productService;
 
     // 상품 생성
-    @PostMapping("/product/save")
+    @PostMapping("/product")
     public ResponseEntity<?> save(ProductResponse.SaveByIdDTO productResponseFind,
                                   @RequestParam MultipartFile[] files) throws IOException {
         productService.save(productResponseFind, files);
@@ -27,7 +24,7 @@ public class ProductController {
     }
 
     // 상품 찾기
-    @PostMapping("/product/{id}")
+    @GetMapping("/product/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         ProductResponse.FindByIdDTO productDTOS = productService.findById(id);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(productDTOS);
@@ -35,14 +32,14 @@ public class ProductController {
     }
 
     // 상품 수정
-    @PostMapping("/product/{id}/update")
+    @PutMapping("/product/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, ProductResponse.FindByIdDTO findByIdDTO) {
         ProductResponse.FindByIdDTO updatedProduct = productService.update(id, findByIdDTO);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(updatedProduct);
         return ResponseEntity.ok(apiResult);
     }
 
-    @PostMapping("/product/{id}/delete")
+    @DeleteMapping("/product/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id){
         productService.delete(id);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(id);
