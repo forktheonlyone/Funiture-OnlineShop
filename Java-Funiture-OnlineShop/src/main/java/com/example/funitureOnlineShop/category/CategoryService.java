ecbfad156fa47114a6e45fb497a883522d6c76f0
mearae <qownsbjh@gmail.com>
2023-12-14 12:17:30 +0900

    [UPDATE] 카테고리 수정 기능 추가
    카테고리의 이름과 그 상위 카테고리 수정

--- Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/category/CategoryService.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/category/CategoryService.java
@@ -16,6 +16,7 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    // 카테고리 저장
     @Transactional
     public void save(CategoryRequest.SaveDto saveDto) {
         // 최상위 카테고리 추가 시
@@ -25,7 +26,7 @@ public class CategoryService {
         } else {
             // 요청 보낸 상위 카테고리가 없을 경우
             Optional<Category> optionalCategory = categoryRepository.findById(saveDto.getSuperCategory_id());
-            if (!optionalCategory.isPresent())
+            if (optionalCategory.isEmpty())
                 throw new Exception404("해당하는 상위 카테고리를 찾을 수 없습니다.");
             // 카테고리 저장
             Category category = categoryRepository.save(saveDto.toEntity());
@@ -33,16 +34,41 @@ public class CategoryService {
         }
     }
 
+    // 등록된 모든 카테고리 탐색
     public List<CategoryResponse.FindAllDto> findAll() {
         List<Category> categories = categoryRepository.findAll();
+
         return categories.stream()
                 .map(CategoryResponse.FindAllDto::new).collect(Collectors.toList());
     }
 
+    // 요청 받은 카테고리와 그 상하위 카테고리 탐색
     public CategoryResponse.FindByIdDto findById(Long id) {
+        // 요청 받은 카테고리
         Category category = categoryRepository.findById(id).orElseThrow(
                 () -> new Exception404("해당 카테고리를 찾을 수 없습니다."));
+        // 하위 카테고리
         List<Category> subCategories = categoryRepository.findBySuperCategoryId(id);
+
         return new CategoryResponse.FindByIdDto(category, subCategories);
+    }
+
+    // 카테고리의 이름과 그 상위 카테고리 수정
+    @Transactional
+    public void update(CategoryRequest.UpdateDto updateDto) {
+        // 수정할 카테고리 존재?
+        categoryRepository.findById(updateDto.getId()).orElseThrow(
+                () -> new Exception404("해당 카테고리를 찾을 수 없습니다."));
+        // 연결한 상위 카테고리 존재?
+        Category superCategory = categoryRepository.findById(updateDto.getSuperCategory_id()).orElseThrow(
+                () -> new Exception404("해당 상위 카테고리를 찾을 수 없습니다."));
+        // 수정 된 카테고리
+        Category newCategory = Category.builder()
+                .id(updateDto.getId())
+                .categoryName(updateDto.getCategoryName())
+                .superCategory(superCategory)
+                .build();
+
+        categoryRepository.save(newCategory);
     }
 }
