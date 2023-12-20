866e954de75dd8eb58b323d1b784cf80a98bfd67
LAH4285 <ahyun4285@gmail.com>
2023-12-20 12:07:07 +0900

    [FIX] 게시판 코드 수정
    게시판 코드 수정

--- Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/home/HomeController.java
+++ Java-Funiture-OnlineShop/src/main/java/com/example/funitureOnlineShop/home/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.funitureOnlineShop.home;
 
+import com.example.funitureOnlineShop.Board.BoardDTO;
+import com.example.funitureOnlineShop.Board.BoardService;
 import com.example.funitureOnlineShop.category.CategoryResponse;
 import com.example.funitureOnlineShop.category.CategoryService;
 import com.example.funitureOnlineShop.core.security.CustomUserDetails;
@@ -13,6 +15,9 @@ import com.example.funitureOnlineShop.productComment.ProductCommentService;
 import com.example.funitureOnlineShop.user.UserResponse;
 import com.example.funitureOnlineShop.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -28,6 +33,7 @@ public class HomeController {
 
     private final ProductService productService;
     private final ProductCommentService productCommentService;
+    private final BoardService boardService;
     private final CategoryService categoryService;
     private final UserService userService;
 
@@ -50,6 +56,20 @@ public class HomeController {
         model.addAttribute("categories", dtos);
 
         return "categoryUpdate";
+    }
+    @GetMapping("/notice")
+    public String noticePaging(@PageableDefault(page = 1) Pageable pageable, Model model) {
+        Page<BoardDTO> boards = boardService.paging(pageable);
+
+        int blockLimit = 3;
+        int startPage = (int) Math.ceil((double) pageable.getPageNumber() / blockLimit - 1) * blockLimit + 1;
+        int endPage = (startPage + blockLimit - 1) < boards.getTotalPages() ? (startPage + blockLimit - 1) : boards.getTotalPages();
+
+        model.addAttribute("boardList", boards);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "noticePage";
     }
 
     // !!----------< 상품 관련 페이지 > -----------
@@ -82,12 +102,6 @@ public class HomeController {
         return "orderPage";
     }
 
-    // !!----------< 고객센터 관련 페이지 > -----------
-    // 공지사항 페이지
-    @GetMapping("/notice")
-    public String showNotice() {
-        return "noticePage";
-    }
 
     // !!----------< 유저 관련 페이지 > -----------
     // 각 유저의 마이페이지
